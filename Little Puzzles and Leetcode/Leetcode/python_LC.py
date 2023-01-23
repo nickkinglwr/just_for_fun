cc09b635d03149f8ad2763d3edb8ee74279951d9
Nick Lower <nicholaskinglower@gmail.com>
2023-01-22 19:39:47 -0800

    Update Python Leetcode file with Add Two Numbers.

--- Little Puzzles and Leetcode/Leetcode/python_LC.py
+++ Little Puzzles and Leetcode/Leetcode/python_LC.py
@@ -8,3 +8,31 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
             tab[n] = i
                 
 
+##### Add Two Numbers #####
+# O(n) time O(1) space
+def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        final = ListNode()
+        curr = final
+        carry = 0
+        
+        while True:
+            if l1 is None and l2 is None:
+                if carry == 1:
+                    curr.val = 1
+                else:
+                    prev.next = None
+                return final
+            
+            elif l1 is None:
+                l1 = ListNode()
+            elif l2 is None:
+                l2 = ListNode()
+            
+            val = l1.val + l2.val + carry
+            curr.val = val % 10
+            carry = val // 10
+            curr.next = ListNode()
+            prev = curr
+            curr = curr.next
+            l1 = l1.next
+            l2 = l2.next
